c7b58fed7180b3f064b09e0e39cc93e45e90265b
jaredthecarrot <jaredmccain.jam@gmail.com>
2025-03-31 14:33:55 -0500

    CARDS IMPORTED ROMANCE SET

--- TCGApp/Main/forms.py
+++ TCGApp/Main/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Cards
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -8,3 +9,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class CardForm(forms.ModelForm):
+    class Meta:
+        model = Cards
+        fields = "__all__"
--- TCGApp/Main/models.py
+++ TCGApp/Main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Cards(models.Model):
+    productId = models.CharField(("productId"), max_length=256)
+    cleanName = models.CharField(("cleanname"), max_length=256)
+    imageUrl = models.CharField(("imageUrl"), max_length=256)
+    url = models.CharField(("url"), max_length=256)
+    lowPrice = models.FloatField(("lowPrice"), )
+    midPrice = models.FloatField(("midPrice"), )
+    highPrice = models.FloatField(("highPrice"), )
+    marketPrice = models.FloatField(("marketPrice"), )
+    subTypeName = models.CharField(("subTypeName"), max_length=256)
+    extNumber = models.CharField(("extNumber"), max_length=256)
+    #extDescription = models.CharField(("extDescription"), max_length=1024)
